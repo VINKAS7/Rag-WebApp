fdd9fb88b0adfe44dca04c6437a71f5b4436e6dd
Vinkas7 <vinayakkashyap753@gmail.com>
2025-08-21 19:06:31 +0530

    added components for Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,19 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Chat from "../components/Chat";
+import SideBar from "../components/SideBar";
 
 function Home(){
     return(
-        <div className="flex flex-col min-h-screen">
-            <Header />
-            <Chat />
-            <Footer />
+        <div className="flex">
+            <SideBar />
+            <div className="flex flex-col min-h-screen">
+                <Header />
+                <Chat />
+                <Footer />
+            </div>
         </div>
+        
     )
 }
 
